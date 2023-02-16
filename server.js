2c66b04368215b9b4134d5e0222453c0b8ad4b2f
cinematografie <kristinadruin@gmail.com>
2023-02-16 18:04:42 +0000

    KD - lesson 1

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
+app.set('views', './views/pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
